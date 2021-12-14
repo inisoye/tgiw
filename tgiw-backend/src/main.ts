bd9da7c98b8812fcbaebca486ec7ec8bcac10027
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-12-14 06:18:44 +0100

    feat(server): Add cookie parser

--- tgiw-backend/src/main.ts
+++ tgiw-backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  app.use(cookieParser());
   const port = configService.get('port');
   await app.listen(port);
 }
