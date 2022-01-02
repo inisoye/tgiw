2b452e0413fe0fba544964a735dc3a60126a4dad
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-02 17:20:23 +0100

    feature: Add edit profile page

---
+++ tgiw-frontend/src/pages/edit-profile.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { MainLayout, NextPageWithLayout } from '@/components/layout';
+
+interface EditProfileProps {}
+
+const EditProfile: NextPageWithLayout = () => {
+  return <div>EditProfile</div>;
+};
+
+export default EditProfile;
