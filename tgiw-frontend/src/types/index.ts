2db521cf51641eb6800109cdfcdc843a0bcb7952
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-03 07:22:23 +0100

    refactor: Add new types

--- tgiw-frontend/src/types/index.ts
+++ tgiw-frontend/src/types/index.ts
@@ -1,3 +1,5 @@
+import type { User as FirebaseUser } from 'firebase/auth';
+
 export interface PaginatedResponse<DataType> {
   data: DataType;
   count: any;
@@ -26,6 +28,13 @@ export interface Genre {
   id: string;
   name: string;
   countries: string[];
+  color: string;
+}
+
+export interface Contributor {
+  id: string;
+  dateAdded: string;
+  userName: string;
 }
 
 export interface Song {
@@ -45,4 +54,47 @@ export interface Song {
   yearReleased: string;
   contributorNote: string;
   genres: Genre[];
+  contributor: Contributor;
+  dateAdded: string;
+}
+
+export interface DbUser {
+  id: string;
+  userName: string;
+  dateAdded: Date;
+  contributions: Song[];
+}
+
+// Added to prevent compiler error when obtaining accessToken
+export interface UserWithAccessToken extends FirebaseUser {
+  stsTokenManager: {
+    accessToken: string;
+  };
+}
+
+export interface ProviderData {
+  providerId: string;
+  uid: string;
+  displayName?: string;
+  email: string;
+  phoneNumber?: string;
+  photoURL?: string;
+}
+export interface StsTokenManager {
+  refreshToken: string;
+  accessToken: string;
+  expirationTime: number;
+}
+
+export interface StoredUser {
+  uid: string;
+  email: string;
+  emailVerified: boolean;
+  isAnonymous: boolean;
+  providerData: ProviderData[];
+  stsTokenManager: StsTokenManager;
+  createdAt: string;
+  lastLoginAt: string;
+  apiKey: string;
+  appName: string;
 }
