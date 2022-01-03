55a4c7b1816f7803d4b4f7303f740a8ef8c6a899
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-03 07:23:32 +0100

    feature: Add get single user functionality

---
+++ tgiw-frontend/src/features/auth/api/getUser.ts
@@ -0,0 +1,23 @@
+import { axios } from '@/config';
+import { useQuery } from 'react-query';
+import type { User as FirebaseUser } from 'firebase/auth';
+
+import type { DbUser } from '@/types';
+
+interface Params {
+  queryKey: (string | { id: string | undefined })[];
+}
+
+export const getUser = async (
+  params: Params
+): Promise<{
+  firebaseUser: FirebaseUser;
+  dbUser: DbUser;
+}> => {
+  const { id } = params.queryKey[1] as { id: string };
+  const { data } = await axios.get(`auth/users/${id}`);
+  return data;
+};
+
+export const useUser = (id: string | undefined) =>
+  useQuery(['user', { id }], getUser);
