eca3ab8e9337acf2664ce8eeb6c76954bb9cf1ec
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-07 23:46:59 +0100

    feature: Add fallback to images

---
+++ tgiw-frontend/src/components/elements/ImageWithFallback.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Image from 'next/image';
+
+interface ImageWithFallbackProps {
+  src: string;
+  fallbackSrc: string;
+  alt: string;
+
+  // All other props
+  [rest: string]: any;
+}
+
+export const ImageWithFallback: React.FunctionComponent<
+  ImageWithFallbackProps
+> = (props) => {
+  const { src, fallbackSrc, alt, ...rest } = props;
+  const [imgSrc, setImgSrc] = React.useState(src || fallbackSrc);
+
+  return (
+    <Image
+      {...rest}
+      src={imgSrc}
+      alt={alt}
+      onError={() => {
+        setImgSrc(fallbackSrc);
+      }}
+    />
+  );
+};
