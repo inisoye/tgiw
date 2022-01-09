8f8592a748eb769e8c5701b7b58c643f7515e4ed
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-09 07:47:18 +0100

    feat: Add bubbles to genre page

--- tgiw-frontend/src/pages/genres/index.tsx
+++ tgiw-frontend/src/pages/genres/index.tsx
@@ -1,29 +1,74 @@
 import * as React from 'react';
 
 import { MainLayout } from '@/components/layout';
-import { AnimatedCircles, useInfiniteGenres } from '@/features/genres';
+import {
+  circleDiameter,
+  GenreLink,
+  getInitialCirclesPositions,
+  getFinalCirclesPositions,
+  useGenres,
+  useWindowSize,
+} from '@/features/genres';
 import { Loader } from '@/components/elements';
-import type { NextPageWithLayout } from '@/types';
+import { Genre, NextPageWithLayout } from '@/types';
 
 interface GenreProps {}
 
 const Genre: NextPageWithLayout = () => {
-  const { data, isLoading, isError, hasNextPage, fetchNextPage } =
-    useInfiniteGenres();
+  const [page, setPage] = React.useState(1);
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useGenres(page);
+  const viewDimensions = useWindowSize();
 
   if (isLoading) {
     return <Loader isFullHeight />;
   }
 
-  const allFetchedGenres = data?.pages
-    ?.map((page) => page.data.map((genre) => genre))
-    .flat();
+  const { data: genres, nextPage, prevPage } = data || {};
+
+  const initialCirclesPositions = getInitialCirclesPositions(
+    viewDimensions,
+    genres?.length as number,
+    circleDiameter
+  );
+
+  const finalCirclesPositions = getFinalCirclesPositions(
+    initialCirclesPositions
+  );
 
   return (
     <div>
-      Genre
-      <div className="relative w-full">
-        <AnimatedCircles />
+      <div className="relative flex items-center justify-center w-full h-screen overflow-hidden">
+        {!!prevPage && (
+          <button onClick={() => setPage((old) => Math.max(old - 1, 0))}>
+            Previous Page
+          </button>
+        )}
+
+        <div className="bg-blue-200 rounded-full w-52 h-52">
+          {genres?.map(({ id, name, color }, index) => (
+            <GenreLink
+              key={id}
+              id={id}
+              name={name}
+              color={color}
+              initialPosition={initialCirclesPositions[index]}
+              finalPosition={finalCirclesPositions[index]}
+            />
+          ))}
+        </div>
+
+        {!!nextPage && (
+          <button
+            onClick={() => {
+              if (!isPreviousData) {
+                setPage((prevPage) => prevPage + 1);
+              }
+            }}
+          >
+            Next Page
+          </button>
+        )}
       </div>
     </div>
   );
