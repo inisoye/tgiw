ee8f78001236bc500ef8d1c8c30acd8f59891d87
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-01-28 15:11:03 +0100

    feat: Add Dialog component

---
+++ tgiw-frontend/src/components/elements/Dialog.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { DialogContent, DialogOverlay } from '@reach/dialog';
+import '@reach/dialog/styles.css';
+import clsx from 'clsx';
+
+interface DialogProps {
+  isDialogOpen: boolean;
+  closeDialog: () => void;
+  isCenterFullHeight?: boolean;
+  isMobileMenu?: boolean;
+}
+
+export const Dialog: React.FunctionComponent<DialogProps> = ({
+  isDialogOpen,
+  closeDialog,
+  isCenterFullHeight,
+  children,
+  isMobileMenu,
+}) => {
+  return (
+    <DialogOverlay
+      className="z-20 "
+      style={{ backgroundColor: 'hsla(0, 0%, 0%, 0.8)' }}
+      isOpen={isDialogOpen}
+      onDismiss={closeDialog}
+    >
+      <div
+        className={clsx(
+          'h-max max-h-[84%] overflow-auto w-screen max-w-3xl mx-auto reach-dialog-content bg-white text-tgiwPurplish',
+          {
+            'h-screen max-h-screen rounded-none': isCenterFullHeight,
+            'fixed bottom-0 md:hidden': isMobileMenu,
+          }
+        )}
+      >
+        <DialogContent
+          aria-label="Happy New Year"
+          /**
+           * Reach UI doesn't work very well with Styled JSX or Tailwind.
+           * Override defaults inline and style containing div instead.
+           */
+          style={{
+            margin: 0,
+            padding: 0,
+            height: '100%',
+            width: '100%',
+            backgroundColor: 'transparent',
+          }}
+        >
+          {children}
+        </DialogContent>
+      </div>
+    </DialogOverlay>
+  );
+};
---
+++ tgiw-frontend/src/hooks/useDialogControl.ts
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export const useDialogControl = (initialState = false) => {
+  const [isDialogOpen, setIsDialogOpen] = React.useState(initialState);
+  const openDialog = () => setIsDialogOpen(true);
+  const closeDialog = () => setIsDialogOpen(false);
+
+  return {
+    isDialogOpen,
+    openDialog,
+    closeDialog,
+  };
+};
+
+
