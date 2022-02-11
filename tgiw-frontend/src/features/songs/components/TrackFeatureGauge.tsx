a358ef64a05e4a805690f2cc5a5140f6c4cfd271
Inioluwa Akinyosoye <inisoye@gmail.com>
2022-02-11 18:09:19 +0100

    fix: Disable sr only text for feature gauge

--- tgiw-frontend/src/features/songs/components/TrackFeatureGauge.tsx
+++ tgiw-frontend/src/features/songs/components/TrackFeatureGauge.tsx
@@ -29,9 +29,9 @@ export const TrackFeatureGauge: React.FunctionComponent<
         </div>
       </div>
 
-      <VisuallyHidden>
+      {/* <VisuallyHidden>
         {featureName} is {roundedValue}%
-      </VisuallyHidden>
+      </VisuallyHidden> */}
 
       <style jsx>{`
         .percentage-gauge {
